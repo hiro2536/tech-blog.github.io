eb71e13920900b55db6b6dd66db4f32c8ef38770
ひろ <h23449817@gmail.com>
2025-08-16 19:24:01 +0900

    setup ogp

--- setting.ts
+++ setting.ts
@@ -1,2 +1,2 @@
 // ex) export const TARGET_URL = "https://xxxx.github.io/tech-blog.github.io/";
-export const TARGET_URL = "https://";
+export const TARGET_URL = "https://hiro2536.github.io/tech-blog.github.io/";
